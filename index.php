d733e33366e09dd5e7500124cddb0aea0659f55d
raja1985 <raja1985@users.noreply.github.com>
2024-06-14 11:01:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	</head>
 
 <body> 
-	<h1>Mayank MOdi</h1>
+	<h1>rajesh</h1>
 		<p><b>Openshift Batch ABB Practical WIth CLI With WebHooks   </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
